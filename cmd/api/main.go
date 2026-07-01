ff43ca9a2f1657d2075070b94f6e52ca012b7372
micsamuel <mykelsown@gmail.com>
2026-07-01 17:10:59 +0100

    Connect the server and handle the localhost run error in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Mykelsown/skinbywura_backend.git/config"
+	"github.com/Mykelsown/skinbywura_backend.git/internal/server"
+)
+
 func main() {
-	
+	serve := server.New(config.Load())
+	fmt.Println("server is running on port 8080")
+	err := serve.Run()
+	if err != nil {
+		log.Fatal("failed to run server on port 8080")
+	}
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,10 +11,10 @@ type stats struct {
 
 func loadRoute() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /api/health", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET ./api/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
-
+		
 		state := stats{"ok"}
 
 		err := json.NewEncoder(w).Encode(state)
