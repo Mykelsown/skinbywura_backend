d0c38fd80d488fb0be822ecd9fdd6d27523ee682
Mykelsown <mykelsown@gmail.com>
2026-07-02 00:07:05 +0100

    Phase 0 of the program completed

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,11 +15,10 @@ func main() {
 		log.Fatalf("failed to load .env file content: %v", err)
 	}
 
-	fmt.Println(config.Load())
 	serve := server.New(config.Load())
-	fmt.Println("server is running on port 8080")
+	fmt.Println("server is running on port "+ config.Load().Port)
 	err  = serve.Run()
 	if err != nil {
-		log.Fatal("failed to run server on port 8080")
+		log.Fatal("failed to run server on port " + config.Load().Port)
 	}
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,7 +11,7 @@ type stats struct {
 
 func loadRoute() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET ./api/health", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		
